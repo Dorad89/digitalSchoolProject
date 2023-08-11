cd1f7c420ddad947651e13357f16f0947e763aac
Dorad89 <doradulqinaku1989@gmail.com>
2023-08-11 22:54:55 +0200

    First Commit

---
+++ src/main/java/org/du/digitalschoolproject/DigitalSchoolProjectApplication.java
@@ -0,0 +1,13 @@
+package org.du.digitalschoolproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalSchoolProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalSchoolProjectApplication.class, args);
+    }
+
+}
